0548ed4d84afe85cdc28071f79fb88a22efacf16
feldim2425 <feldim2425@gmail.com>
2021-05-14 15:31:44 +0200

    Some comments for Common.hpp

--- src/common/Common.hpp
+++ src/common/Common.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+/**
+ * Namespace for OpenCamoCap
+ */
 #define CAMOCAP_NAMESPACE ocamocap
+
+/**
+ * Macro to open the OpenCamoCap Namespace
+ */
 #define CAMOCAP_NAMESPACE_BEGIN namespace CAMOCAP_NAMESPACE {
+
+/**
+ * Macro to close the OpenCamoCap Namespace
+ */
 #define CAMOCAP_NAMESPACE_END }
